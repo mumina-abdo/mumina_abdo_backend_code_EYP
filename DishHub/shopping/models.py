9cca4748b2258ada2e6cb89b1345529064cedb5a
Mercy-Chemtai <158445902+Mercy-Chemtai@users.noreply.github.com>
2024-09-18 16:44:30 +0300

    Update models.py

--- DishHub/shopping/models.py
+++ DishHub/shopping/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class Shopping_list(models.Model):
+class ShoppingList(models.Model):
     shopping_list_id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return f"{self.shopping_list_id}"
 
 
-class Shopping_list_item(models.Model):
+class ShoppingListItem(models.Model):
     shopping_list_item_id = models.AutoField(primary_key=True)
     shopping_list_id = models.ForeignKey(Shopping_list, on_delete=models.CASCADE, related_name='items') 
     item = models.CharField(max_length=255, default="item")
