88d86cd3ceacc2d2ec5e140f9ea1fab8980e9797
Mercy-Chemtai <158445902+Mercy-Chemtai@users.noreply.github.com>
2024-09-19 14:20:18 +0300

    Delete DishHub/shopping/admin.py

--- DishHub/shopping/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Shopping_list, Shopping_list_item
-
-# Register your models here.
-admin.site.register(Shopping_list)
-admin.site.register(Shopping_list_item)
